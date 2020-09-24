0a795b8869b020ced267c8be26854c77573d76e7
Nick Watson <ngwatson79@yahoo.com>
2020-09-24 19:23:01 -0400

    Final Product - Stretch 1 completed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function createMenuItem(name, price, category) {
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 console.log(createMenuItem("Lasagna", 25, "Dinner"))
-console.log(createMenuItem("Mega Omlette", 15, "Breakfast"))
+console.log(createMenuItem("Mega Omelette", 15, "Breakfast"))
 console.log(createMenuItem("Meatloaf Sandwich", 17, "Lunch"))
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
@@ -51,7 +51,7 @@ console.log(burger.discount("teacher"))
 const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
     {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
     {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly recommend."},
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
@@ -103,12 +103,14 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
+
 function getLastReview(array) {
 
     console.log(`${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating}, and their feedback was ${array[array.length - 1].feedback}`);
   } 
 
 getLastReview(reviews);
+
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
 /** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept: 
@@ -119,7 +121,7 @@ getLastReview(reviews);
   and should return an array of objects. 
 
   For example, invoking getReviewByRating(reviews, 4) would return [{name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly recommend."},
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
@@ -146,15 +148,23 @@ Your function should accept:
 and should return an array of objects. 
 
   For example, invoking getLongReviews(reviews) would return [
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly recommend."},
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-  function getLongReviews(/* code here */) {
-    /* code here */
+  function getLongReviews(array) {
+
+      const reviewList2 = [];
+      let wordCount = 0;
+      for (let i = 0; i , getLongReviews.length; i++);
+      if (array[i] === " ") {
+      wordCount +1;
+  } if (wordCount >= 15) {
+    reviewList2.push();
+  } console.log(reviewList2);
   }
   
-
+getLongReviews(reviews);
 /* STRETCH 3:  This challenge is not related to the data above! 
 
 Write a function called carMarker 
